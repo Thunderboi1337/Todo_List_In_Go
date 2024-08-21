5e02d5f32a8f684e7138eb3a66daa5fa1eb07581
Marcus Robertsson <marcus.asberg.98@gmail.com>
2024-08-21 10:19:24 +0200

    Made better, forgot I was opening the file in the read function, So if I call the function multiple time, it will open the file every time

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"os"
 )
@@ -13,42 +12,39 @@ type Employee struct {
 }
 
 func main() {
-
 	var csvFile *os.File
 
 	if _, err := os.Stat("todo_list.csv"); os.IsNotExist(err) {
 		// File does not exist, create it
-		csvFile, err := os.Create("todo_list.csv")
+		csvFile, err = os.Create("todo_list.csv")
 		if err != nil {
 			log.Fatalf("failed creating file: %s", err)
 		}
 		defer csvFile.Close() // Ensure the file is closed after creation
 	} else if err != nil {
 		log.Fatalf("error checking file: %s", err)
+	} else {
+		// File exists, open it
+		csvFile, err = os.Open("todo_list.csv")
+		if err != nil {
+			log.Fatalf("failed opening file: %s", err)
+		}
+		defer csvFile.Close() // Ensure the file is closed after reading
 	}
 
+	// Pass the file to the read_todo function
 	read_todo(csvFile)
-
 }
 
-func write_todo() {
-
-}
-
+// Function that accepts the file as an argument
 func read_todo(file *os.File) {
-
-	file, err := os.Open("todo_list.csv")
-	if err != nil {
-		log.Fatalf("failed reading file: %s", err)
-	}
-
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		// Print each line in the file
+		log.Println(scanner.Text())
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatalf("error reading file: %s", err)
 	}
-
 }
